e560216b691a7c204f941cfe81641669400b9d1b
fabio abrantes diniz <fabio.abrantes.diniz@gmail.com>
2022-05-06 02:25:35 -0300

    versão 2 do projeto PDM

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,14 @@
 import express from 'express';
+import "reflect-metadata";
+import './database';
+
 
 const app = express();
+app.use(express.json());
+
+
+
+app.listen(3000, ()=>{
+  console.log("listening on port 3000");
+});
 
